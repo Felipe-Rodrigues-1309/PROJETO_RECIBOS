8bf40a7dc55f9053b027882e9ff9680f8c7ef4ae
felipe <feliphi10@gmail.com>
2026-04-22 17:22:18 -0300

    bootstrap

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ $funcionario = $conn->query("SELECT * FROM funcionarios ORDER BY nome ASC ");
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/recibos.css" />
     <script src="js/index.js"></script>
     <link
@@ -29,6 +26,7 @@ $funcionario = $conn->query("SELECT * FROM funcionarios ORDER BY nome ASC ");
       type="image/x-icon"
     />
 
+    <a href=""></a>
     <title>Recibo</title>
   </head>
   <body>
